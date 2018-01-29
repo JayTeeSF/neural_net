325fae4351ff39aac6af2a982206c6c700fe04e4
JayTeeSF <jonathan@his-service.net>
2018-01-29 00:09:56 -0800

    Updated README; MNIST handwritten digit data (csv) to json (neuralnet specification) converter

--- README.txt
+++ README.txt
@@ -4,10 +4,30 @@ Create a .json file, e.g.:
 Then run it as a neural net:
 ./bin/neural_net.rb ./data/or_gate.json
 
-# FIXME:
+# Download MNIST csv(s):
+  https://pjreddie.com/media/files/mnist_train.csv
+  https://pjreddie.com/media/files/mnist_test.csv
+
+
+# Train on MNIST data:
+↪ time ./bin/mnist_csv_to_json.rb train
+↪ ./bin/neural_net.rb ./data/mnist_train.json
+
+# FIXME: this depends on being able to save the trained net?!
+# Test MNIST 
+↪ time ./bin/mnist_csv_to_json.rb test
+↪ ./bin/neural_net.rb ./data/mnist_test.json
+
+# TODO:
   update input to be closer to output, i.e.:
   ./output/initial_unbiased_or_gate.json
 
+  a)
   # include (optional) connections & weights
   # that way a trained net can be saved and restored for use later...
 
+  b)
+  # activation function specified (by name)
+
+  c)
+  # visualize output (not just JSON, but html/css OR canvas)
---
+++ bin/mnist_csv_to_json.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+DATA_FILE_FMT = "#{__dir__}/../data/mnist_%s.csv".freeze
+
+if ARGV[0] =~ /help/
+  puts "#{$0} [help|train|test] [<max_avg_err 0.1>] [hidden_layer_sizes 15]"
+  exit
+end
+
+file_type = ARGV.pop
+if file_type =~ /train/
+  file_type = "train"
+  csv_data_file = DATA_FILE_FMT % "train"
+else
+  file_type = "test"
+  csv_data_file = DATA_FILE_FMT % "test"
+end
+
+max_avg_err = (ARGV.pop || 0.1).to_f
+hidden_layer_sizes = ARGV
+if hidden_layer_sizes.empty?
+  hidden_layer_sizes = [15]
+end
+
+warn "Using the '%s' data file: #{csv_data_file}" % file_type
+
+json_file_name = "#{__dir__}/../data/mnist_%s.json" % file_type
+warn "Outputting network description to file: #{json_file_name}"
+
+def to_target(expected_digit)
+  case expected_digit.to_i
+  when 0
+    [1,0,0,0,0,0,0,0,0,0]
+  when 1
+    [0,1,0,0,0,0,0,0,0,0]
+  when 2
+    [0,0,1,0,0,0,0,0,0,0]
+  when 3
+    [0,0,0,1,0,0,0,0,0,0]
+  when 4
+    [0,0,0,0,1,0,0,0,0,0]
+  when 5
+    [0,0,0,0,0,1,0,0,0,0]
+  when 6
+    [0,0,0,0,0,0,1,0,0,0]
+  when 7
+    [0,0,0,0,0,0,0,1,0,0]
+  when 8
+    [0,0,0,0,0,0,0,0,1,0]
+  else
+    [0,0,0,0,0,0,0,0,0,1]
+  end
+end
+
+File.open(json_file_name, "w") {|f|
+  f.puts <<-EOTIPTOP
+{
+  "bias_enabled": false,
+  "topology": [
+    784,
+  EOTIPTOP
+
+  f.puts hidden_layer_sizes.join(",\n") + ",\n"
+
+  f.puts <<-EOTOP
+    10
+  ],
+  "max_avg_sq_error": #{max_avg_err},
+  "inputs": [
+  EOTOP
+
+  expected_digits = []
+  first_time = true
+  CSV.foreach(csv_data_file) do |row|
+    if !first_time
+      f.puts ","
+    end
+    expected_digits << row[0]
+    f.print "[" + row[1..-1].join(", ") + "]"
+    if first_time
+      first_time = false
+    end
+  end
+
+  f.puts <<-EOMID
+  ],
+  "targets": [
+  EOMID
+
+  f.puts "[\n" + expected_digits.map{|d| to_target(d).join(",\n") }.join("\n],\n[\n") + "\n]"
+  f.puts <<-EOBOTTOM
+  ]}
+  EOBOTTOM
+}
+
+warn "done."
